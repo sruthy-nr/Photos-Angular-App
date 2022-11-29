939c2019951b73a8f8c3479d346ef480a829df7b
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-29 15:46:12 +0530

    viewphotos

---
+++ src/app/view-photo/view-photo.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-view-photo',
+  templateUrl: './view-photo.component.html',
+  styleUrls: ['./view-photo.component.css']
+})
+export class ViewPhotoComponent {
+constructor(private api:ApiService){
+  api.fetchPhoto().subscribe(
+    (response)=>
+    {
+      this.photo=response;
+    }
+  )
+}
+photo:any=[]
+}
