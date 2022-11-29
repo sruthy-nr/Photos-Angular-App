ee62f154531cd5a7c3a7c3bb5dfb2d1260215879
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-29 16:47:10 +0530

    development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPhotoComponent } from './view-photo/view-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
